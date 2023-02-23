836d3e8bb7df7ed8f0023c6d02a9f6687448ca77
Nicklas <nicklas9211@gmail.com>
2023-02-23 15:02:14 +0100

    react-app, typescript and tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+    return (
+        <div className=" flex justify-center bg-slate-400 min-h-screen ">
+            <div className=" bg-slate-500 rounded-md h-11 ">
+                temp
+            </div>
+            ddd
+        </div>
+    );
+}
+
+export default App;

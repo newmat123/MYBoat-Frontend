4b1822934e6161e3df1446719dff2b3ea0d72d66
Nicklas <nicklas9211@gmail.com>
2023-02-23 17:35:17 +0100

    simple app UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 
 function App() {
+
+    let currentTemp = 25;
+    let maxTemp = 27;
+
+    let currentHumidity = 10;
+    let maxHumidity = 20;
+
+    let waterInBilge = true;
+
     return (
-        <div className=" flex justify-center bg-slate-400 min-h-screen ">
-            <div className=" bg-slate-500 rounded-md h-11 ">
-                temp
+        <div className="flex justify-center bg-slate-400 min-h-screen">
+
+            <div className="mt-24 ">
+
+                <div className="border-b-2 border-slate-700 min-w-fit md:min-w-[500px] p-4 text-center">
+                    <h1 className="text-5xl font-bold">BÅDMANEGER</h1>
+                </div>
+
+                <div className="flex flex-col justify-center">
+
+                    {
+                        waterInBilge &&
+                        <div className="mt-12 text-center bg-red-600 rounded-md p-5 m-5">
+                            <h3 className="text-4xl font-bold text-gray-200">Der Er Vand I Kølen</h3>
+                            <div className="">
+                                <h1 className="text-2xl font-bold text-gray-200">Warning</h1>
+                                <span className="text-gray-300">Skynd dig at få styr på den</span>
+                            </div>
+                        </div>
+                    }
+
+
+                    <div className="mt-12 text-center">
+                        <h3 className="text-4xl font-bold text-gray-700">Temperature {currentTemp} c<sup>o</sup></h3>
+                        <div className="my-8">
+                            <h1 className="text-2xl font-bold text-gray-800">Top {maxTemp} c<sup>o</sup></h1>
+                            <span className="text-gray-500">Højeste temp i dag</span>
+                        </div>
+                        {/* <canvas>
+
+                        </canvas> */}
+                    </div>
+
+                    <div className="mt-12 text-center">
+                        <h3 className="text-4xl font-bold text-gray-700">Fugt {currentHumidity} c<sup>o</sup></h3>
+                        <div className="my-8">
+                            <h1 className="text-2xl font-bold text-gray-800">Top {maxHumidity} c<sup>o</sup></h1>
+                            <span className="text-gray-500">Højeste fugt i dag</span>
+                        </div>
+                        {/* <canvas>
+
+                        </canvas> */}
+                    </div>
+
+                </div>
+
             </div>
-            ddd
+
         </div>
     );
 }

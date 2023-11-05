291b7306e3d93d5a50a88b65ed1867f273093133
Nicklas <nicklas9211@gmail.com>
2023-11-05 22:58:07 +0100

    fix: Round response value

--- src/apis/clientAPI.ts
+++ src/apis/clientAPI.ts
@@ -1,5 +1,16 @@
+import { environment_ } from "../controllers/pages/main.controller";
 import { api } from "./configs/axiosConfig"
 import { defineCancelApiObject } from "./configs/axiosUtils"
+
+const roundNumber = (response: environment_) => {
+    if (response === undefined || response.value === undefined || typeof response.value === "boolean" || response.value2 === undefined) {
+        return response;
+    }
+    var fixedResponse = response;
+    fixedResponse.value = Math.round(response.value * 100) / 100
+    fixedResponse.value2 = Math.round(response.value2 * 100) / 100
+    return fixedResponse;
+}
 
 export const ClientApi = {
     getTemperature: async function (cancel = false) {
@@ -10,27 +21,23 @@ export const ClientApi = {
             signal: cancel ? cancelApiObject[this.getTemperature.name].handleRequestCancellation().signal : undefined,
         })
         // returning the product returned by the API
-        return response.data
+        return roundNumber(response.data);
     },
     getHeat: async function (cancel = false) {
         const response = await api.request({
             url: `/heat`,
             method: "GET",
-            // retrieving the signal value by using the property name
             signal: cancel ? cancelApiObject[this.getHeat.name].handleRequestCancellation().signal : undefined,
         })
-        // returning the product returned by the API
-        return response.data
+        return roundNumber(response.data);
     },
     getHumidity: async function (cancel = false) {
         const response = await api.request({
             url: `/humidity`,
             method: "GET",
-            // retrieving the signal value by using the property name
             signal: cancel ? cancelApiObject[this.getHumidity.name].handleRequestCancellation().signal : undefined,
         })
-        // returning the product returned by the API
-        return response.data
+        return roundNumber(response.data);
     },
     getEnvironment: async function (cancel = false) {
         const response = await api.request({
@@ -38,7 +45,6 @@ export const ClientApi = {
             method: "GET",
             signal: cancel ? cancelApiObject[this.getEnvironment.name].handleRequestCancellation().signal : undefined,
         })
-
         return response.data
     },
     getWifiStatus: async function (cancel = false) {
@@ -47,7 +53,6 @@ export const ClientApi = {
             method: "GET",
             signal: cancel ? cancelApiObject[this.getWifiStatus.name].handleRequestCancellation().signal : undefined,
         })
-
         return response.data.value
     },
     getBilgeStatus: async function (cancel = false) {
@@ -56,7 +61,6 @@ export const ClientApi = {
             method: "GET",
             signal: cancel ? cancelApiObject[this.getBilgeStatus.name].handleRequestCancellation().signal : undefined,
         })
-
         return response.data.value
     },
     postWifiCredentials: async function (ssid_: string, pwd_: String, cancel = false) {
@@ -72,7 +76,6 @@ export const ClientApi = {
             },
             signal: cancel ? cancelApiObject[this.postWifiCredentials.name].handleRequestCancellation().signal : undefined,
         })
-
         return response.data
     },
 }
